3447f4ac282c668d9841b82ca92e4fde8b6e5f19
Florian <florod631@gmail.com>
2017-07-28 09:57:59 +0200

    Fin de l'exercice 3

--- src/TestArrayTri.java
+++ src/TestArrayTri.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class TestArrayTri {
 
@@ -5,11 +6,13 @@ public class TestArrayTri {
 		// TODO Auto-generated method stub
 		
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+		int[] array2 = new int [array.length];
 		int tampon = 0;
 		
 		//affichage du tableau original
 		for (int i=0; i<array.length; i++){ 
 			System.out.print(array[i] + "\t");
+			array2 [i] = array[i];
 		}
 		
 		// tri par insertion, méthode destructive
@@ -24,10 +27,19 @@ public class TestArrayTri {
 		}
 		
 		//affichage du tableau trié
-		System.out.println("\nLe tableau trié est le suivant: ");
+		System.out.println("\nLe tableau 1 trié est le suivant: ");
 		for (int i=0; i<array.length; i++){ 
 			System.out.print(array[i] + "\t");
 		}
+		
+		//tri avec la méthode sort
+		Arrays.sort(array2);
+		
+		System.out.println("\nLe tableau 2 trié est le suivant: ");
+		for (int i=0; i<array2.length; i++){ 
+			System.out.print(array2[i] + "\t");
+		}
+		
 	}
 
 }
